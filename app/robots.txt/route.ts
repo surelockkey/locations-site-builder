8d731c83b8112201aff9ab55cc327bc98ff5769e
SureLock Key Dev <devmain@surelockkey.com>
2026-02-25 14:26:19 +0200

    merge

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -9,12 +9,6 @@ Allow: /
 Disallow: /api/
 Disallow: /_next/
 
-# SEO Directives
-Follow: all
-Index: all
-Max-snippet: -1
-Max-video-preview: -1
-Max-image-preview: large
 
 Sitemap: ${baseUrl}/sitemap.xml`;
 
